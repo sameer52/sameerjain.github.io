6b474b6f40f918629938effa84c1ffcf2b6ca9e9
Sameer Jain <sameerj5252@gmail.com>
2026-01-06 17:14:53 -0500

    various small changes

--- assets/js/header-scroll-animation.js
+++ assets/js/header-scroll-animation.js
@@ -1,8 +1,23 @@
-window.addEventListener('scroll', function() {
-    const header = document.getElementById('header-nav');
-    if (window.scrollY > 50) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-});
+// js/headerScroll.js
+
+import { getHeader } from './dom.js';
+
+export const handleScroll = () => {
+  const header = getHeader();
+  if (!header) return;
+
+  if (window.scrollY > 50) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+};
+
+// window.addEventListener('scroll', function() {
+//     const header = document.getElementById('header-nav');
+//     if (window.scrollY > 50) {
+//         header.classList.add('scrolled');
+//     } else {
+//         header.classList.remove('scrolled');
+//     }
+// });
